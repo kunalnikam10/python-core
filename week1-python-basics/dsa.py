aa91e4850c23e07e415124c48fd108746c22d287
Kunal Nikam <89775251+kunalnikam10@users.noreply.github.com>
2026-02-15 21:40:34 +0530

    Day 6: Dsa Practice

--- week1-python-basics/dsa.py
+++ week1-python-basics/dsa.py
@@ -25,8 +25,24 @@ class Solution:
         return k
     
 nums= [3,2,2,3]
-sol =Solution()
-print(sol.removeElement(nums, 3))
+# sol =Solution()
+# print(sol.removeElement(nums, 3))
+
+class Solution2:
+    def isPalindrome(self, x: int) -> bool:
+        num= x
+        reverse=0
+        while(num>0):
+            reverse = (num%10) + (reverse*10)
+            num = num//10
+
+        if(reverse== x):
+            return True
+        else:
+            return False
+
+obj= Solution2()
+print(obj.isPalindrome(1212))
   
 
 l1= [2,5,3,5,6,3,11,7,8,14,4,2]
